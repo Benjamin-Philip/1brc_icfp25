3dfc4e1322032f4b375c0634a1044a579eadf08f
Benjamin Philip <benjamin.philip495@gmail.com>
2024-11-21 17:19:12 +0530

    Initialize Repo

---
+++ lib/ibrc.ex
@@ -0,0 +1,18 @@
+defmodule Ibrc do
+  @moduledoc """
+  Documentation for `Ibrc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ibrc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ibrc_test.exs
@@ -0,0 +1,8 @@
+defmodule IbrcTest do
+  use ExUnit.Case
+  doctest Ibrc
+
+  test "greets the world" do
+    assert Ibrc.hello() == :world
+  end
+end
